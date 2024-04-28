a13d71dcb3176b6324d626cd06e3e882ddf5a6af
Griffin Yourick <gryphon92@gmail.com>
2024-04-28 17:39:11 -0400

    fix option

--- exercises/options/options2.rs
+++ exercises/options/options2.rs
@@ -26,10 +26,7 @@ mod tests {
 
         let mut cursor = range;
 
-        // TODO: make this a while let statement - remember that vector.pop also
-        // adds another layer of Option<T>. You can stack `Option<T>`s into
-        // while let and if let.
-        if let Some(Some(integer)) = optional_integers.pop() {
+        while let Some(Some(integer)) = optional_integers.pop() {
             assert_eq!(integer, cursor);
             cursor -= 1;
         }
