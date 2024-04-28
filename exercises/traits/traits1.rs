08a13d28106a618b4b65593d0be5646121354cbe
Griffin Yourick <gryphon92@gmail.com>
2024-04-28 17:59:55 -0400

    traits

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -7,14 +7,16 @@
 // Execute `rustlings hint traits1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+use std::fmt::format;
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 impl AppendBar for String {
-    // TODO: Implement `AppendBar` for type `String`.
+    fn append_bar(self) -> String {
+        format!("{self}Bar")
+    }
 }
 
 fn main() {
